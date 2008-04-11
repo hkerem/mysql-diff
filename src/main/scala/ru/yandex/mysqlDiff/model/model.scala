c87219d6f4455df36fd01f34d2c7de79e8458c51
mq0 <none@none>
2008-04-11 16:23:05 +0400

    [svn r566] add and modify models of SQL objects
    --HG--
    branch : trunk

--- src/main/scala/ru/yandex/mysqlDiff/model/model.scala
+++ src/main/scala/ru/yandex/mysqlDiff/model/model.scala
@@ -1,17 +1,41 @@
 package ru.yandex.mysqlDiff.model
 
-case class DataType(val name: String, val length: Option[Int]) {
-    
+import scala.collection.mutable._
+
+class DataType(val name: String, val length: Option[Int]) {
+    override def toString = "" + name + "[" + length.getOrElse("default") + "]" 
 }
 
-case class Column(val name: String, val dataType: DataType) {
-    
+class ColumnModel(val name: String, val dataType: DataType) {
+  val isNotNull: boolean = false
+  val indexes: Map[String, IndexModel] = new HashMap()
+}
+
+class ConstraintModel(val name: String ) {
+  
+}
+
+class IndexModel(override val name: String, val columns: Seq[ColumnModel], isUnique: boolean) 
+       extends ConstraintModel(name: String) 
+{
+  
+}
+
+class ForeighKey(override val name: String, 
+       val localColumns: Seq[ColumnModel], 
+       val externalTable: TableModel,
+       val externalColumns: Seq[ColumnModel])
+       extends ConstraintModel(name: String)
+{
+     
 }
 
 abstract class DatabaseDeclaration
 
-case class Table(val name: String, val columns: Seq[Column]) extends DatabaseDeclaration {
-    
+class TableModel(val name: String, val columns: Seq[ColumnModel]) extends DatabaseDeclaration {
+  val columnsMap: Map[String, ColumnModel] = new HashMap()
+  val primaryKey: List[ColumnModel] = null;
+  val constraints: List[ConstraintModel] = null;
 }
 
-case class Database(val declarations: Seq[DatabaseDeclaration])
+class DatabaseModel(val declarations: Seq[DatabaseDeclaration])
