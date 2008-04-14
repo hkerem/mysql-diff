b480db1d531fb38c5353e7c129bfe941cd7af174
mq0 <none@none>
2008-04-14 12:09:03 +0400

    [svn r568] code beauty
    --HG--
    branch : trunk

--- src/main/scala/ru/yandex/mysqlDiff/model/diff.scala
+++ src/main/scala/ru/yandex/mysqlDiff/model/diff.scala
@@ -2,10 +2,14 @@ package ru.yandex.mysqlDiff.model
 
 abstract class DiffType
 
-case class NameDiff(val oldName: String, val newName: String) extends DiffType
+case class NameDiff(val oldName: String, val newName: String) 
+        extends DiffType
 
-case class TypeDiff(val oldType: DataType, val newType: DataType) extends DiffType 
+case class TypeDiff(val oldType: DataType, val newType: DataType) 
+        extends DiffType 
 
-case class ColumnDiff(val oldColunm: ColumnModel, val newColumn: ColumnModel, val diffList: Seq[DiffType]) extends DiffType
+case class ColumnDiff(val oldColunm: ColumnModel, val newColumn: ColumnModel, val diffList: Seq[DiffType]) 
+        extends DiffType
 
-case class TableDiff(val oldTable: TableModel, val newTable: TableModel, val diffList: Seq[DiffType]) extends DiffType
+case class TableDiff(val oldTable: TableModel, val newTable: TableModel, val diffList: Seq[DiffType]) 
+        extends DiffType
