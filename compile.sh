2fb6e7dfecf478c152aaf7dbe10dfd456dcef5bd
mq0 <none@none>
2008-04-15 20:40:02 +0400

    [svn r580] add test and scalaX library
    --HG--
    branch : trunk

--- compile.sh
+++ compile.sh
@@ -2,13 +2,14 @@
 
 mkdir -p target/classes
 
-
 if [ "$SCALA_HOME" = "" ]
 then
     echo 'Set SCALA_HOME environment variable to your scala jdk path'
     exit 1
 fi
 
-$SCALA_HOME/bin/scalac -Xprint:refchecks -deprecation -d target/classes src/main/scala/**/*.scala
+
+#-Xprint:refchecks 
+$SCALA_HOME/bin/scalac -deprecation -classpath lib/**/*.jar -d target/classes src/main/scala/**/*.scala 
 
 # vim: set ts=4 sw=4 et:
