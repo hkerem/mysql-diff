c66abd95b516e7a8b79b595c435a252fb632ec51
mq0 <none@none>
2008-04-15 19:08:01 +0400

    [svn r578] add SCALA_HOME environment variable use to compile script
    --HG--
    branch : trunk

--- compile.sh
+++ compile.sh
@@ -2,6 +2,13 @@
 
 mkdir -p target/classes
 
-/opt/lang/java/JDK/scala-2.7.0-final/bin/scalac -deprecation -d target/classes src/main/scala/**/*.scala
+
+if [ "$SCALA_HOME" = "" ]
+then
+    echo 'Set SCALA_HOME environment variable to your scala jdk path'
+    exit 1
+fi
+
+$SCALA_HOME/bin/scalac -Xprint:refchecks -deprecation -d target/classes src/main/scala/**/*.scala
 
 # vim: set ts=4 sw=4 et:
