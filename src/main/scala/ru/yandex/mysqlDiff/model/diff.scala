400ab43277bacd8486245518adb18235798aa263
mq0 <none@none>
2008-04-29 19:14:11 +0400

    [svn r711] some comments
    --HG--
    branch : trunk

--- src/main/scala/ru/yandex/mysqlDiff/model/diff.scala
+++ src/main/scala/ru/yandex/mysqlDiff/model/diff.scala
@@ -2,23 +2,29 @@ package ru.yandex.mysqlDiff.model
 
 abstract class DiffType(val from: SqlObjectType, val to: SqlObjectType)
 
+
+//name of object diff
 case class NameDiff[A <: SqlObjectType](override val from: A, override val to:A) 
     extends DiffType(from, to)
 
+//diff of column type
 case class DataTypeDiff(override val from: ColumnModel, override val to: ColumnModel) 
     extends DiffType(from, to)
  
-
+//diff of column nullable
 case class NotNullDiff(override val from: ColumnModel, override val to: ColumnModel)
     extends DiffType(from, to)
 
+//diff of autoincrement
 case class AutoIncrementDiff(override val from: ColumnModel, override val to: ColumnModel)
     extends DiffType(from, to)
 
+//diff of default value
 case class DefaultValueDiff(override val from: ColumnModel, override val to:ColumnModel) 
     extends DiffType(from, to)
 
-//keys diff
+//keys diff:
+
 case class PrimaryKeyDiff(override val from: PrimaryKeyModel, override val to: PrimaryKeyModel)
     extends DiffType(from, to)
 
@@ -38,7 +44,7 @@ case class FulltextKeyDiff(override val from: IndexModel, override val to: Index
     extends DiffType(from, to)
 //end keys diff
 
-
+//diff container
 abstract class DiffContainter[A <: SqlObjectType, B <: DiffType]
     (override val from: A, override val to:A, val diffList : Seq[B])
     extends DiffType(from: A, to: A)
