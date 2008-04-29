cf3e119e0a5ca1fb52d2b7fa3a8279d5d17451b4
nga <none@none>
2008-04-29 11:53:41 +0400

    [svn r670] s/MySqlDiffTest/MysqlDiffTest
    --HG--
    branch : trunk

--- src/main/scala/ru/yandex/mysqlDiff/MysqlDiffTest.scala
+++ src/main/scala/ru/yandex/mysqlDiff/MysqlDiffTest.scala
@@ -2,7 +2,7 @@ package ru.yandex.mysqlDiff;
 
 import scalax.testing._
 
-object MySqlDiffTest extends TestSuite("MySQL diff full test") {
+object MysqlDiffTest extends TestSuite("MySQL diff full test") {
   include(ru.yandex.mysqlDiff.diff.simple.SimpleDiffTest);
   include(ru.yandex.mysqlDiff.diff.simple.ScriptBulderTest);
   include(ru.yandex.mysqlDiff.diff.simple.SimpleTextHarvesterTest);
