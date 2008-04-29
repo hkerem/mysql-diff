fb6365fd22c368d0ee554487067eca8facfdc0f8
mq0 <none@none>
2008-04-29 13:04:32 +0400

    [svn r685] CAP-28: space changes
    --HG--
    branch : trunk

--- src/test/sql/simple_to.sql
+++ src/test/sql/simple_to.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS complaints (
-    id INT (11) AUTO_INCREMENT,
-    user_id BIGINT NOT NULL,
-    file_id INT NOT NULL,
-    r_id BIGINT NOT NULL,
-    created_time TIMESTAMP DEFAULT NOW(),
-    message VARCHAR(8192) NOT NULL default '',
-    PRIMARY KEY(user_id, file_id, reporter_id)
+   id INT (11) AUTO_INCREMENT,
+   user_id BIGINT NOT NULL,
+   file_id INT NOT NULL,
+   r_id BIGINT NOT NULL,
+   created_time TIMESTAMP DEFAULT NOW(),
+   message VARCHAR(8192) NOT NULL default '',
+   PRIMARY KEY(user_id, file_id, reporter_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS test (
-    id INT NOT NULL,
-    test1 VARCHAR(1000),
-    blim INT
+   id INT NOT NULL,
+   test1 VARCHAR(1000),
+   blim INT
 );
 
