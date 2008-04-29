c0f3b245a5c2893278da62b364cb04a6ab046ca5
mq0 <none@none>
2008-04-29 14:42:45 +0400

    [svn r695] fix compile error
    --HG--
    branch : trunk

--- src/main/scala/ru/yandex/mysqlDiff/MysqlDiffTest.scala
+++ src/main/scala/ru/yandex/mysqlDiff/MysqlDiffTest.scala
@@ -5,5 +5,5 @@ import scalax.testing._
 object MysqlDiffTest extends TestSuite("MySQL diff full test") {
     include(ru.yandex.mysqlDiff.diff.simple.SimpleDiffTest);
     include(ru.yandex.mysqlDiff.diff.simple.ScriptBulderTest);
-    include(ru.yandex.mysqlDiff.diff.simple.SimpleTextHarvesterTest);
+    include(ru.yandex.mysqlDiff.diff.simple.TextParserTest);
 }
