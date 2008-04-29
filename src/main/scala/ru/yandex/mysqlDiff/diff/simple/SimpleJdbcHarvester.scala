7bb263bc0a4ac5c05ccb5256669d2280e45fef79
mq0 <none@none>
2008-04-29 13:09:17 +0400

    [svn r687] CAP-28: space changes
    --HG--
    branch : trunk

--- src/main/scala/ru/yandex/mysqlDiff/diff/simple/SimpleJdbcHarvester.scala
+++ src/main/scala/ru/yandex/mysqlDiff/diff/simple/SimpleJdbcHarvester.scala
@@ -8,118 +8,116 @@ import ru.yandex.mysqlDiff.model._
 object SimpleJdbcHarvester {
 
 
-    def parseTable(tables: ResultSet, data: DatabaseMetaData): TableModel = {
-        val tableName = tables.getString("TABLE_NAME");
-        val columns = data.getColumns(null, null, tableName, "%");
-        var columnsList = List[ColumnModel]()
+   def parseTable(tables: ResultSet, data: DatabaseMetaData): TableModel = {
+       val tableName = tables.getString("TABLE_NAME");
+       val columns = data.getColumns(null, null, tableName, "%");
+       var columnsList = List[ColumnModel]()
 
-        while (columns.next) {
-            val colName = columns.getString("COLUMN_NAME");
-            val colType = columns.getString("TYPE_NAME");
-            var colTypeSize: int = -1;
-            if (columns.getObject("COLUMN_SIZE") != null) colTypeSize = columns.getInt("COLUMN_SIZE");
+       while (columns.next) {
+           val colName = columns.getString("COLUMN_NAME");
+           val colType = columns.getString("TYPE_NAME");
+           var colTypeSize: int = -1;
+           if (columns.getObject("COLUMN_SIZE") != null) colTypeSize = columns.getInt("COLUMN_SIZE");
 
-            val isNotNull = !columns.getString("IS_NULLABLE").equalsIgnoreCase("yes");
-            val isAutoinrement = columns.getString("IS_AUTOINCREMENT").equalsIgnoreCase("YES");
-
-
-            var typeSizeOption: Option[int] = None;
-            if (colTypeSize  != -1) typeSizeOption = Some(colTypeSize)
-
-            val cm = new ColumnModel(colName, new DataType(colType, typeSizeOption))
-            cm.isNotNull = isNotNull
-            cm.isAutoIncrement = isAutoinrement
-
-            columnsList = (columnsList ++ List(cm)).toList
-        }
-
-        val tm  = new TableModel(tableName, columnsList)
-        columnsList.foreach(x => x.parent = tm)
-        tm
-    }
+           val isNotNull = !columns.getString("IS_NULLABLE").equalsIgnoreCase("yes");
+           val isAutoinrement = columns.getString("IS_AUTOINCREMENT").equalsIgnoreCase("YES");
 
 
-    def parsePrimaryKeys(table: TableModel, data: DatabaseMetaData): PrimaryKeyModel = {
+           var typeSizeOption: Option[int] = None;
+           if (colTypeSize  != -1) typeSizeOption = Some(colTypeSize)
 
-        var pkColumns = List[String]()
+           val cm = new ColumnModel(colName, new DataType(colType, typeSizeOption))
+           cm.isNotNull = isNotNull
+           cm.isAutoIncrement = isAutoinrement
 
-        var pkName = ""
-
-        val primaryKeys: ResultSet = data.getPrimaryKeys(null, null, table.name)
-        while (primaryKeys.next) {
-            val column = primaryKeys.getString("COLUMN_NAME")
-            val indexName = primaryKeys.getString("PK_NAME")
-            pkColumns = (pkColumns ++ List(column)).toList
-            pkName = indexName
-        }
-
-        if (pkColumns.size > 0) {
-            var pk = new PrimaryKeyModel(pkName, pkColumns);
-            pk.parent = table;
-            table.primaryKey = pk;
-            pk
-        } else null
-    }
+           columnsList = (columnsList ++ List(cm)).toList
+       }
+       val tm  = new TableModel(tableName, columnsList)
+       columnsList.foreach(x => x.parent = tm)
+       tm
+   }
 
 
+   def parsePrimaryKeys(table: TableModel, data: DatabaseMetaData): PrimaryKeyModel = {
 
-    def parseIndexes(table: TableModel, data: DatabaseMetaData, checkPrimaryKey: boolean): Seq[IndexModel] = {
-        var indexesMap: Map[String, List[String]] = Map()
-        val indexes = data.getIndexInfo(null, null, table.name, false, true)
-        while (indexes.next) {
-            val colName = indexes.getString("COLUMN_NAME");
-            val indexName = indexes.getString("INDEX_NAME");
-            if (!checkPrimaryKey || table.primaryKey == null ||
-                table.primaryKey.name == null || !table.primaryKey.name.equals(indexName))
+       var pkColumns = List[String]()
 
-            if (indexesMap.contains(indexName))
-                indexesMap(indexName) = (indexesMap(indexName) ++ List(colName)).toList
-            else
-                indexesMap(indexName) = List(colName)
-        }
+       var pkName = ""
 
-        val resultList = indexesMap.map(x => new IndexModel(x._1, x._2, false)).filter(x => {x.parent = table; true})
-        if (table.keys == null)
-            table.keys = resultList.toList
-        else
-            table.keys = (table.keys ++ resultList.toList).toList
-        resultList.toList
-    }
+       val primaryKeys: ResultSet = data.getPrimaryKeys(null, null, table.name)
+       while (primaryKeys.next) {
+           val column = primaryKeys.getString("COLUMN_NAME")
+           val indexName = primaryKeys.getString("PK_NAME")
+           pkColumns = (pkColumns ++ List(column)).toList
+           pkName = indexName
+       }
+
+       if (pkColumns.size > 0) {
+           var pk = new PrimaryKeyModel(pkName, pkColumns);
+           pk.parent = table;
+           table.primaryKey = pk;
+           pk
+       } else null
+   }
 
 
 
-    def parseIndexes(table: TableModel, data: DatabaseMetaData): Seq[IndexModel] = {
-        parseIndexes(table, data, false)
-    }
+   def parseIndexes(table: TableModel, data: DatabaseMetaData, checkPrimaryKey: boolean): Seq[IndexModel] = {
+       var indexesMap: Map[String, List[String]] = Map()
+       val indexes = data.getIndexInfo(null, null, table.name, false, true)
+       while (indexes.next) {
+           val colName = indexes.getString("COLUMN_NAME");
+           val indexName = indexes.getString("INDEX_NAME");
+           if (!checkPrimaryKey || table.primaryKey == null ||
+               table.primaryKey.name == null || !table.primaryKey.name.equals(indexName))
+
+           if (indexesMap.contains(indexName))
+               indexesMap(indexName) = (indexesMap(indexName) ++ List(colName)).toList
+           else
+               indexesMap(indexName) = List(colName)
+       }
+
+       val resultList = indexesMap.map(x => new IndexModel(x._1, x._2, false)).filter(x => {x.parent = table; true})
+       if (table.keys == null)
+           table.keys = resultList.toList
+       else
+           table.keys = (table.keys ++ resultList.toList).toList
+       resultList.toList
+   }
 
 
-    def parseUnique(table: TableModel, data: DatabaseMetaData): Seq[IndexModel] = {
-        parseIndexes(table, data, true)
-    }
+
+   def parseIndexes(table: TableModel, data: DatabaseMetaData): Seq[IndexModel] = {
+       parseIndexes(table, data, false)
+   }
 
 
-    def search(url: String): Seq[TableModel] = {
-        Class.forName("com.mysql.jdbc.Driver").newInstance;
-
-        val conn: Connection = DriverManager.getConnection(url);
-        val data: DatabaseMetaData = conn.getMetaData;
-
-        val tables: ResultSet = data.getTables(null, "%", "%", List("TABLE").toArray);
-
-        var returnTables = List[TableModel]()
-
-        while (tables.next) {
-            val tableModel = parseTable(tables, data);
-            val pk = parsePrimaryKeys(tableModel, data)
-            val indexes = parseIndexes(tableModel, data);
-            val unique = parseUnique(tableModel, data);
-
-            returnTables = (returnTables ++ List(tableModel)).toList
-        }
-        returnTables
-    }
+   def parseUnique(table: TableModel, data: DatabaseMetaData): Seq[IndexModel] = {
+       parseIndexes(table, data, true)
+   }
 
 
-    def parse(connectionString: String): DatabaseModel = new DatabaseModel("database", search(connectionString))
+   def search(url: String): Seq[TableModel] = {
+       Class.forName("com.mysql.jdbc.Driver").newInstance;
+
+       val conn: Connection = DriverManager.getConnection(url);
+       val data: DatabaseMetaData = conn.getMetaData;
+
+       val tables: ResultSet = data.getTables(null, "%", "%", List("TABLE").toArray);
+
+       var returnTables = List[TableModel]()
+
+       while (tables.next) {
+           val tableModel = parseTable(tables, data);
+           val pk = parsePrimaryKeys(tableModel, data)
+           val indexes = parseIndexes(tableModel, data);
+           val unique = parseUnique(tableModel, data);
+           returnTables = (returnTables ++ List(tableModel)).toList
+       }
+       returnTables
+   }
+
+
+   def parse(connectionString: String): DatabaseModel = new DatabaseModel("database", search(connectionString))
 
 }
