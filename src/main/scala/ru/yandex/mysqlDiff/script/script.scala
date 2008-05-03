cf32b3ef6a9b545194af0297182501ce58201f6f
nga <none@none>
2008-05-03 20:20:00 +0400

    [svn r729] more full script
    --HG--
    branch : trunk

--- src/main/scala/ru/yandex/mysqlDiff/script/script.scala
+++ src/main/scala/ru/yandex/mysqlDiff/script/script.scala
@@ -8,4 +8,18 @@ abstract class ScriptStatement
 
 case class CreateTableStatement(model: TableModel) extends ScriptStatement
 
+case class UnparsedStatement(q: String) extends ScriptStatement
+
+case class AlterTableStatement(tableName: String, op: AlterTableStatement.Operation)
+
+object AlterTableStatement {
+    abstract class Operation
+    
+    case class AddColumn(model: ColumnModel) extends Operation
+    
+    case class ChangeColumn(oldName: String, model: ColumnModel) extends Operation
+    
+    case class ModifyColumn(model: ColumnModel) extends Operation
+}
+
 // vim: set ts=4 sw=4 et:
