cb5e5e0bdd012e97756f7abcab85f9c1ae35fd07
nga <none@none>
2008-05-04 01:05:43 +0400

    [svn r748] fix pizdets
    --HG--
    branch : trunk

--- mysql-diff.sh
+++ mysql-diff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -classpath ./target/dist/mysqlDiff-0.0.jar:./target/dist/scala-library.jar:./target/dist/mysql-connector-java-5.1.6-bin.jar ru.yandex.mysqlDiff.Diff $1 $2
+java -classpath ./target/dist/mysqlDiff-0.0.jar:./target/dist/scala-library.jar:./target/dist/mysql-connector-java-5.1.6-bin.jar ru.yandex.mysqlDiff.Diff "$@"
 
