f447f37075f101b67ff81d1d88c738a960a7d485
nga <none@none>
2008-05-03 20:31:32 +0400

    [svn r731] script...
    --HG--
    branch : trunk

---
+++ src/main/scala/ru/yandex/mysqlDiff/script/ScriptSerializer.scala
@@ -0,0 +1,7 @@
+package ru.yandex.mysqlDiff.script
+
+object ScriptSerializer {
+    def serializeStatement(stmt: ScriptStatement) = ""
+}
+
+// vim: set ts=4 sw=4 et:
--- src/main/scala/ru/yandex/mysqlDiff/script/script.scala
+++ src/main/scala/ru/yandex/mysqlDiff/script/script.scala
@@ -2,13 +2,17 @@ package ru.yandex.mysqlDiff.script
 
 import model._
 
-class Script(val stmts: Seq[ScriptStatement])
+class Script(val stmts: Seq[ScriptElement])
 
-abstract class ScriptStatement
+abstract class ScriptElement
 
-case class CreateTableStatement(model: TableModel) extends ScriptStatement
+case class CommentStatement(comment: String) extends ScriptElement
+
+abstract class ScriptStatement extends ScriptElement
 
 case class UnparsedStatement(q: String) extends ScriptStatement
+
+case class CreateTableStatement(model: TableModel) extends ScriptStatement
 
 case class AlterTableStatement(tableName: String, op: AlterTableStatement.Operation)
 
