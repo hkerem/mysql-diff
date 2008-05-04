2795340c9906e301d17fa3c23fea4cc92a2916e5
mq0 <none@none>
2008-05-04 18:26:50 +0400

    [svn r768] CAP-60:
    --HG--
    branch : trunk

--- src/main/scala/ru/yandex/mysqlDiff/Diff.scala
+++ src/main/scala/ru/yandex/mysqlDiff/Diff.scala
@@ -15,33 +15,32 @@ object Diff {
         "mysqlDiff.sh file_name_from|jdbc:jdbc_url_to_source  file_name_to|jdbc:jdbc_url_to_destination"
 
     def main(args: Array[String]) = {
-        if (args.length < 2) System.err.println(helpBanner) else
-            if (args.length > 2) System.err.println(helpBanner) else {
-                val fromArgs = args(0)
-                val toArgs = args(1)
-                if (fromArgs == null || fromArgs.trim.equals("") || toArgs == null|| toArgs.trim.equals("")) {
-                    System.err.println(helpBanner)
-                } else {
-                    try {
-                        var fromdb = getModelFromArgsLine(args(0))
-                        var todb = getModelFromArgsLine(args(1))
-                        Console.println("-- start diff script from " + fromArgs  + " to " + toArgs + "\n")
+        if (args.length != 2) System.err.println(helpBanner) else {
+            val fromArgs = args(0)
+            val toArgs = args(1)
+            if (fromArgs == null || fromArgs.trim.equals("") || toArgs == null|| toArgs.trim.equals("")) {
+                System.err.println(helpBanner)
+            } else {
+                try {
+                    var fromdb = getModelFromArgsLine(args(0))
+                    var todb = getModelFromArgsLine(args(1))
+                    Console.println("-- start diff script from " + fromArgs  + " to " + toArgs + "\n")
 
-                        val dbDiff = DatabaseDiffMaker.doDiff(fromdb, todb)
+                    val dbDiff = DatabaseDiffMaker.doDiff(fromdb, todb)
 
-                        val script = DiffSerializer.serialize(fromdb, todb, dbDiff)
+                    val script = DiffSerializer.serialize(fromdb, todb, dbDiff)
 
-                        println(script)
+                    println(script)
 
-                        Console.println("-- end of diff script from " + fromArgs  + " to " + toArgs + "\n")
-                    } catch {
-                        case e: MysqlDiffException => {
-                            System.err.println("An error while diff building")
-                            System.err.println(e.message)
-                        }
+                    Console.println("-- end of diff script from " + fromArgs  + " to " + toArgs + "\n")
+                } catch {
+                    case e: MysqlDiffException => {
+                        System.err.println("An error while diff building")
+                        System.err.println(e.message)
                     }
                 }
             }
+        }
     }
 
     def getModelFromArgsLine(arg: String): DatabaseModel = {
