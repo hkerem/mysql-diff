7de048d61e06ee9c03db4f593f6a69d9a6e23017
mq0 <none@none>
2008-05-04 14:02:21 +0400

    [svn r751] CAP-54: move all data reading to one method
    --HG--
    branch : trunk

--- src/main/scala/ru/yandex/mysqlDiff/Diff.scala
+++ src/main/scala/ru/yandex/mysqlDiff/Diff.scala
@@ -14,60 +14,42 @@ object Diff {
         "How to use:\n" +
         "mysqlDiff.sh file_name_from|jdbc:jdbc_url_to_source  file_name_to|jdbc:jdbc_url_to_destination\n"
 
-    def main(args: Array[String]) {
+    def main(args: Array[String]) = {
         if (args.length < 2) {
             Console.println(helpBanner)
-            return
-        }
-
-        val fromArgs = args(0)
-        val toArgs = args(1)
-
-        if (fromArgs == null || fromArgs.trim.equals("") || toArgs == null|| toArgs.trim.equals("")) {
-            Console.println(helpBanner)
-            return
-        }
-
-        var fromdb: DatabaseModel = null
-        var todb: DatabaseModel = null
-
-
-        if (fromArgs.toLowerCase.startsWith("jdbc:")) {
-            val fromUrl = fromArgs.trim
-            fromdb = JdbcModelExtractor.parse(fromUrl)
         } else {
-            val from = new File(args(0))
-            if (!from.isFile) {
-                Console.println("\"From\" file is not a file.")
-                return
-            }
-            var fromStr = ""
-            Source.fromFile(from).getLines.foreach(x => {fromStr = fromStr + x})
-            fromdb = ScriptParser.parseModel(fromStr)
-        }
 
-        if (toArgs.toLowerCase.startsWith("jdbc:")) {
-            val toUrl = toArgs.trim
-            todb = JdbcModelExtractor.parse(toUrl)
-        } else {
-            val to = new File(args(1))
-            if (!to.isFile) {
-                Console.println("\"To\" file is not a file.")
-                return;
-            }
-            var toStr = ""
-            Source.fromFile(to).getLines.foreach(x => {toStr = toStr + x})
-            todb = ScriptParser.parseModel(toStr)
-        }
+            val fromArgs = args(0)
+            val toArgs = args(1)
+            if (fromArgs == null || fromArgs.trim.equals("") || toArgs == null|| toArgs.trim.equals("")) {
+                Console.println(helpBanner)
+            } else {
 
-        Console.println("-- start diff script from " + fromArgs  + " to " + toArgs + "\n");
+                var fromdb = getModelFromArgsLine(args(0))
+                var todb = getModelFromArgsLine(args(1))
 
-        val dbDiff = DatabaseDiffMaker.doDiff(fromdb, todb)
+                Console.println("-- start diff script from " + fromArgs  + " to " + toArgs + "\n");
+
+                val dbDiff = DatabaseDiffMaker.doDiff(fromdb, todb)
         
-        val script = DiffSerializer.serialize(fromdb, todb, dbDiff)
+                val script = DiffSerializer.serialize(fromdb, todb, dbDiff)
 
-		println(script)
+                println(script)
 
-        Console.println("-- end of diff script from " + fromArgs  + " to " + toArgs + "\n");
+                Console.println("-- end of diff script from " + fromArgs  + " to " + toArgs + "\n");
+            }
+        }
     }
+
+    def getModelFromArgsLine(arg: String): DatabaseModel = {
+        if (arg.toLowerCase.startsWith("jdbc:")) JdbcModelExtractor.parse(arg.trim)
+            else {
+                val sourceF = new File(arg)
+                if (!sourceF.isFile)  throw new RuntimeException("\"" + arg + "\" file is not a file.")
+                var str = ""
+                Source.fromFile(sourceF).getLines.foreach(x => {str = str + x})
+                ScriptParser.parseModel(str)
+            }
+    }
+
 }
