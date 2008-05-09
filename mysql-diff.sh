947c256a4374b2da38c7afb95376c512fd35b129
nga <none@none>
2008-05-09 04:06:04 +0400

    [svn r859] use scala to run mysql-diff
    --HG--
    branch : trunk

--- mysql-diff.sh
+++ mysql-diff.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
-java -classpath ./target/mysqlDiff-0.0.jar ru.yandex.mysqlDiff.Diff "$@"
+#!/bin/sh -e
 
+scala -classpath 'target/classes:lib/*' ru.yandex.mysqlDiff.Diff "$@"
+
+# vim: set ts=4 sw=4 et:
