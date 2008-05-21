af52877706a645c5cb0c7271cb32807c4b522669
nga <none@none>
2008-05-21 18:05:40 +0400

    [svn r940] model MysqlServerVersion
    --HG--
    branch : trunk

---
+++ src/main/scala/ru/yandex/mysqlDiff/vendor/mysql/mysql.scala
@@ -0,0 +1,38 @@
+package ru.yandex.mysqlDiff.vendor.mysql
+
+case class MysqlServerVersion(major: Int, minor: Int, third: Int) extends Ordered[MysqlServerVersion] {
+    override def compare(that: MysqlServerVersion) = {
+        if (this.major != that.major) this.major - that.major
+        else if (this.minor != that.minor) this.minor - that.minor
+        else this.third - that.third
+    }
+    
+    override def toString = major + "." + minor + "." + third
+}
+
+object MysqlServerVersion {
+    private val VERSION_REGEX = """(\d+)\.(\d+)\.(\d+).*""".r
+    
+    def parse(versionString: String) =
+        versionString match {
+            case VERSION_REGEX(major, minor, third) => MysqlServerVersion(major.toInt, minor.toInt, third.toInt)
+        }
+}
+
+object MysqlServerVersionTests extends org.specs.Specification {
+    "compare" in {
+        MysqlServerVersion(6, 0, 0) > MysqlServerVersion(5, 2, 0) must beTrue
+        MysqlServerVersion(5, 2, 1) > MysqlServerVersion(5, 2, 0) must beTrue
+        MysqlServerVersion(5, 2, 1) compare MysqlServerVersion(5, 2, 1) must_== 0
+    }
+    
+    "parse" in {
+        MysqlServerVersion.parse("5.0.51a-3-log") must_== MysqlServerVersion(5, 0, 51)
+    }
+}
+
+object MysqlTests extends org.specs.Specification {
+    include(MysqlServerVersionTests)
+}
+
+// vim: set ts=4 sw=4 et:
---
+++ src/main/scala/ru/yandex/mysqlDiff/vendor/vendors.scala
@@ -0,0 +1,7 @@
+package ru.yandex.mysqlDiff.vendor
+
+object VendorTests extends org.specs.Specification {
+    include(mysql.MysqlTests)
+}
+
+// vim: set ts=4 sw=4 et:
