313ba585856967f9a8f65acb5c549a4f168cca3b
nga <none@none>
2008-06-20 14:09:31 +0400

    [svn r1058] -Xss1m
    --HG--
    branch : trunk

--- scala-with-classpath.sh
+++ scala-with-classpath.sh
@@ -2,6 +2,6 @@
 
 # Developer script to run scala with specified classpath
 
-scala -classpath 'target/classes:lib/*' "$@"
+JAVA_OPTS=-Xss10m scala -classpath 'target/classes:lib/*' "$@"
 
 # vim: set ts=4 sw=4 et:
