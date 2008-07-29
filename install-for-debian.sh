429d24c8057f87e48a22ce16d96b4f378bdbf486
nga <none@none>
2008-07-29 12:56:47 +0400

    [svn r1158] deb
    --HG--
    branch : trunk

--- install-for-debian.sh
+++ install-for-debian.sh
@@ -1,8 +1,18 @@
 #!/bin/sh -e
 
+errExit() {
+    echo "$@" >&2
+    exit 1
+}
+
 ant dist
 
-echo "incomplete yet" >&2
-exit 1
+DESTDIR="$1"
+
+[ -n "$DESTDIR" ] || errExit "usage: $0 <destdir>"
+
+mkdir -p $DESTDIR/usr/local
+
+cp -a target/dist/mysql-diff $DESTDIR/usr/local/mysql-diff
 
 # vim: set ts=4 sw=4 et:
