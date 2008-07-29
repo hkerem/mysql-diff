b9c31fb7e016fbb67cda2572ee04b8bcbb516fe0
nga <none@none>
2008-07-30 00:09:32 +0400

    [svn r1175] can be executed from any dir
    --HG--
    branch : trunk

--- scala-with-classpath.sh
+++ scala-with-classpath.sh
@@ -2,6 +2,6 @@
 
 # Developer script to run scala with specified classpath
 
-JAVA_OPTS=-Xss10m scala -classpath 'target/classes:lib/*' "$@"
+JAVA_OPTS=-Xss10m scala -classpath `dirname $0`/target/classes:`dirname $0`/lib/'*' "$@"
 
 # vim: set ts=4 sw=4 et:
