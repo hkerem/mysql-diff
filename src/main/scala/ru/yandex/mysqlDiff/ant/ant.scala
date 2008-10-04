6f46e2f0d3e25ae5be5588a8566db9f89f053ff2
stepancheg@mx1.ru <stepancheg@mx1.ru>
2008-10-04 19:39:46 +0400

    Ant task (closes #3)
    --HG--
    branch : trunk

---
+++ src/main/scala/ru/yandex/mysqlDiff/ant/ant.scala
@@ -0,0 +1,35 @@
+package ru.yandex.mysqlDiff.ant
+
+import org.apache.tools.ant._
+import java.io.File
+import scalax.io.Implicits._
+
+import diff._
+
+class DiffTask extends Task {
+    private var from: String = _
+    private var to: String = _
+    private var output: File = _
+    
+    def setFrom(from: String) { this.from = from; }
+    def setTo(to: String) { this.to = to; }
+    def setOutput(output: File) { this.output = output; }
+    
+    override def execute() {
+        if (from eq null) throw new IllegalStateException("required parameter 'from' not set")
+        if (to eq null) throw new IllegalStateException("required parameter 'to' not set")
+        if (output eq null) throw new IllegalStateException("required parameter 'output' not set")
+        
+        val fromModel = Utils.getModelFromArgsLine(from)
+        val toModel = Utils.getModelFromArgsLine(to)
+        
+        val dbDiff = DiffMaker.compareDatabases(fromModel, toModel)
+        val script = DiffSerializer.serialize(fromModel, toModel, dbDiff)
+        
+        log("Writing diff to " + output)
+        output.write(script)
+    }
+
+} //~
+
+// vim: set ts=4 sw=4 et:
