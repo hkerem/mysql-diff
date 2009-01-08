9ac9b5f6a469d94cbb81c74d70ca047b8fd6a694
Stepan Koltsov <stepancheg@yandex-team.ru>
2009-01-08 22:46:02 +0300

    instructions to access test database
    --HG--
    branch : trunk

--- src/main/scala/ru/yandex/mysqlDiff/vendor/postgresql/tests.scala
+++ src/main/scala/ru/yandex/mysqlDiff/vendor/postgresql/tests.scala
@@ -11,6 +11,15 @@ import util._
 object PostgresqlTestDataSourceParameters extends TestDataSourceParameters {
     //Class.forName("")
     
+    /// to create database
+    // CREATE DATABASE mysql_diff_tests
+    // CREATE USER test
+    // ALTER ROLE test WITH PASSWORD 'test'
+    // GRANT ALL ON DATABASE mysql_diff_tests TO test
+    
+    /// connect to database from console
+    // PGPASSWORD=test psql --host=localhost --user test mysql_diff_tests
+    
     override val testDsUrl = "jdbc:postgresql:mysql_diff_tests"
     override val testDsUser = "test"
     override val testDsPassword = "test"
