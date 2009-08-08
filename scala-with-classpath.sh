ced88c44f30b43f408d6a124035afd8dc774ec34
Stepan Koltsov <stepancheg@yandex-team.ru>
2009-08-09 00:59:14 +0400

    scala-with-classpath can be interfactive again

--- scala-with-classpath.sh
+++ scala-with-classpath.sh
@@ -2,6 +2,14 @@
 
 # Developer script to run scala with specified classpath
 
-JAVA_OPTS=-Xss10m java -classpath `dirname $0`/target/classes:`dirname $0`/lib/dep/'*' "$@"
+base() {
+    JAVA_OPTS=-Xss10m java -classpath `dirname $0`/target/classes:`dirname $0`/lib/dep/'*' "$@"
+}
+
+if [ "$#" = 0 ]; then
+    base scala.tools.nsc.MainGenericRunner -nocompdaemon
+else
+    base "$@"
+fi
 
 # vim: set ts=4 sw=4 et:
