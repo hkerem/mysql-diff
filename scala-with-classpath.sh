ded6186ff48a19317922835ce3dc8358dfa3de55
Stepan Koltsov <yozh@mx1.ru>
2009-09-13 17:03:12 +0400

    force JDK 1.6 on MacOS X

--- scala-with-classpath.sh
+++ scala-with-classpath.sh
@@ -2,8 +2,20 @@
 
 # Developer script to run scala with specified classpath
 
+if [ `uname` = Darwin ]; then
+    JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
+fi
+
+javacmd() {
+    if [ -z $JAVA_HOME ]; then
+        java "$@"
+    else
+        "$JAVA_HOME/bin/java" "$@"
+    fi
+}
+
 base() {
-    JAVA_OPTS=-Xss10m java -classpath `dirname $0`/target/classes:`dirname $0`/lib/dep/'*' "$@"
+    JAVA_OPTS=-Xss10m javacmd -classpath `dirname $0`/target/classes:`dirname $0`/lib/dep/'*' "$@"
 }
 
 if [ "$#" = 0 ]; then
